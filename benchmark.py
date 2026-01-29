9c2da73a9d36b3d0be7a4f4ea114a9794a47c731
Jinwon Pyo <gch99517@gmail.com>
2026-01-30 00:42:01 +0900

    Add perplexity measure and use spda

--- benchmark.py
+++ benchmark.py
@@ -26,22 +26,38 @@ config = LlamaConfig(
 )
 
 # Default attention 구현인 eager 모드 사용
-config._attn_implementation = "eager"
+# config._attn_implementation = "eager"
 
 # 벤치마크 하이퍼파라미퍼 (부하 조절용)
 PROMPT_LEN = 1024  # 입력 시퀀스 길이
 GEN_LEN = 512     # 생성할 토큰 수
-BATCH_SIZE = 2      # 배치 크기
+BATCH_SIZE = 1      # 배치 크기
 WARMUP_STEPS = 1   # 워밍업 스텝 수
 TEST_STEPS = 3   # 측정 스텝 수
 
 dummy_input = torch.randint(0, config.vocab_size, (BATCH_SIZE, PROMPT_LEN)).to(DEVICE)
 attention_mask = torch.ones_like(dummy_input).to(DEVICE)
 
+def calculate_perplexity(model, input_ids):
+    """
+    주어진 입력 시퀀스에 대한 Perplexity(PPL)를 계산합니다.
+    PPL = exp(CrossEntropyLoss)
+    """
+    model.eval()
+    
+    with torch.no_grad():
+        outputs = model(input_ids, labels=input_ids)
+        loss = outputs.loss
+        
+    ppl = torch.exp(loss)
+    return ppl.item()
+
 def benchmark(model):
     # 원본 Llama 모델 생성 및 GPU로 이동
     model = model.to(DEVICE).half()
     model.eval()
+
+    ppl = calculate_perplexity(model, dummy_input)
 
     print("Warming up...")
     for _ in range(WARMUP_STEPS):
@@ -63,6 +79,7 @@ def benchmark(model):
     end_event = torch.cuda.Event(enable_timing=True)
 
     first_token_id = -1
+    last_token_id = -1
 
     for step in range(TEST_STEPS):
         torch.cuda.synchronize()
@@ -82,6 +99,7 @@ def benchmark(model):
         torch.cuda.synchronize()
 
         if step == 0:
+            first_token_id = output[0, PROMPT_LEN].item()
             last_token_id = output[0, -1].item()
 
         elapsed_time_ms = start_event.elapsed_time(end_event)  # 밀리초 단위
@@ -90,38 +108,50 @@ def benchmark(model):
 
     avg_latency_sec = sum(latencies) / len(latencies) / 1000  # 초 단위로 변환
 
-    return avg_latency_sec, last_token_id  # 평균 지연 시간과 마지막 생성 토큰 ID 반환
+    return avg_latency_sec, ppl, (first_token_id, last_token_id)  # 평균 지연 시간과 마지막 생성 토큰 ID 반환
 
 def main():
     print("[Native Llama Model]")
     native_model = LlamaForCausalLM(config)
     state_dict = native_model.state_dict()
 
-    # 입력 데이터 생성
-    dummy_input = torch.randint(0, config.vocab_size, (BATCH_SIZE, PROMPT_LEN)).to(DEVICE)
-
-    native_avg_latency, native_last_token = benchmark(native_model)
+    native_avg_latency, native_ppl, native_token_ids = benchmark(native_model)
     del native_model
     clean_gpu_memory()
 
     print("[Custom Llama Model]")
     custom_model = CustomLlamaForCausalLM(config)
     msg = custom_model.load_state_dict(state_dict, strict=False)
+
     print(f"Weight Loading Result: {msg}")
 
-    custom_avg_latency, custom_last_token = benchmark(custom_model)
+    custom_avg_latency, custom_ppl, custom_token_ids = benchmark(custom_model)
     del custom_model
     clean_gpu_memory()
 
-    if native_last_token == custom_last_token:
-        print(f"Last generated token matches: {native_last_token}")
-    else:
-        print(f"Last generated token mismatch! Native: {native_last_token}, Custom: {custom_last_token}")
+    '''
+        First/Last Token ID 비교
+
+        첫번째 토큰은 동일하나 마지막 토큰은 부동소수점 연산의 non-determinism으로 인해 다를 수 있습니다.
+        
+        이는 KV 캐시에 누적된 미세한 오차가 결국 생성 결과에 영향을 미치기 때문입니다.
+
+        만약 두 모델의 PPL이 거의 동일하다면 이는 모델이 본질적으로 유사하게 작동함을 의미합니다.
+
+        또한 현재 빈 깡통 모델을 사용하기 때문에 Perplexity는 아주 높은 값을 가집니다.
+    '''
+    if native_token_ids != custom_token_ids:
+        print("Token generation mismatch between native and custom models")
+        print(f"Native first/last token IDs: {native_token_ids}")
+        print(f"Custom first/last token IDs: {custom_token_ids}")
 
     print("\n[Benchmark Results]")
     print(f"Native Model Avg Latency: {native_avg_latency:.2f} seconds")
     print(f"Custom Model Avg Latency: {custom_avg_latency:.2f} seconds")
     print(f"Speedup: {native_avg_latency / custom_avg_latency:.2f}x")
+    print(f"Native Model Perplexity: {native_ppl:.2f}")
+    print(f"Custom Model Perplexity: {custom_ppl:.2f}")
+    print(f"Perplexity Difference in %: {(abs(native_ppl - custom_ppl) / native_ppl) * 100:.6f}%")
 
 if __name__ == "__main__":
     main()
